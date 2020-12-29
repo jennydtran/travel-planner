2d499ae6a82267708087e37c96975276cc8bfee8
Jenny Tran <jdtran.art@gmail.com>
2020-12-29 23:05:58 +0000

    setState of numberOfNights gets updated correctly through callback function

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,8 +1,185 @@
 import React from 'react';
 
-export default function Home(props) {
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalView: false,
+      tripEntries: []
+    };
+    this.setState = this.setState.bind(this);
+    this.handleClickAdd = this.handleClickAdd.bind(this);
+  }
+
+  handleClickAdd() {
+    this.setState({
+      modalView: true
+    });
+  }
+
+  render() {
+    const { modalView, tripEntries } = this.state;
+    const { handleClickAdd } = this;
+    return (
+      <>
+        <TopNav />
+        <HomeBody />
+        <BottomNav onClick={handleClickAdd}/>
+        { modalView &&
+          <TripModal view={modalView} tripEntries={tripEntries} homeState={this.setState}/>
+        }
+      </>
+    );
+  }
+}
+
+function TopNav(props) {
   return (
-    <>
-    </>
+    <nav className="nav static-fixed-top navbar-light justify-content-end align-items-center">
+      <div className="navbar-brand abs">
+        <svg className="icon" data-name="Livello 1" id="Livello_1" width="2rem" height="2rem" fill="000000" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg">
+          <path d="M127.12,60.22,115.46,48.56h0L69,2.05a7,7,0,0,0-9.9,0L12.57,48.53h0L.88,60.22a3,3,0,0,0,4.24,4.24l6.57-6.57V121a7,7,0,0,0,7,7H46a7,7,0,0,0,7-7V81a1,1,0,0,1,1-1H74a1,1,0,0,1,1,1v40a7,7,0,0,0,7,7h27.34a7,7,0,0,0,7-7V57.92l6.54,6.54a3,3,0,0,0,4.24-4.24ZM110.34,121a1,1,0,0,1-1,1H82a1,1,0,0,1-1-1V81a7,7,0,0,0-7-7H54a7,7,0,0,0-7,7v40a1,1,0,0,1-1,1H18.69a1,1,0,0,1-1-1V51.9L63.29,6.29a1,1,0,0,1,1.41,0l45.63,45.63Z" />
+        </svg>
+      </div>
+      <a href="#" className="float-right">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" fill="000000" className="icon bi bi-three-dots-vertical" viewBox="0 0 16 16">
+          <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+        </svg>
+      </a>
+    </nav>
   );
 }
+
+function HomeBody(props) {
+  return (
+    <main className="home">
+      <div className="homeWelcome">
+        <p className="message">Hello, <strong>username</strong></p>
+        <p className="message"><span>What are we planning today?</span></p>
+        <hr />
+      </div>
+
+      {/* <TripEntries /> */}
+
+      {/* if no entries... render below */}
+      <div className="text-center"><h3>Looks like you don&#39;t<br />have any trips planned!</h3></div>
+      <div className="text-center"><p>Let&#39;s start planning!<br />Click the <strong>ADD</strong> button to add a trip.</p></div>
+    </main>
+  );
+}
+
+function BottomNav(props) {
+  return (
+    <footer className="footer static-fixed-bottom">
+      <div className="text-center">
+        <div className="icon add" onClick={props.onClick}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16">
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+          </svg>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+class TripModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tripName: '',
+      tripDestination: '',
+      departureDate: '',
+      returnDate: '',
+      numberOfNights: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClickOff = this.handleClickOff.bind(this);
+    this.calculateNights = this.calculateNights.bind(this);
+  }
+
+  calculateNights() {
+    const date1 = Date.parse(this.state.departureDate);
+    const date2 = Date.parse(this.state.returnDate);
+    const timeDiff = date2 - date1;
+    const nights = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+    this.setState(state => ({
+      numberOfNights: nights
+    }));
+  }
+
+  handleChange(event) {
+    const value = event.target.value;
+    this.setState({
+      [event.target.name]: value
+    }, this.calculateNights);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const req = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    };
+    fetch('/api/trip', req)
+      .then(response => response.json())
+      .then(newTrip => {
+        const newTripEntriesArray = this.props.tripEntries.concat(newTrip);
+        this.props.homeState({ tripEntries: newTripEntriesArray });
+        event.target.reset();
+      })
+      .catch(err => console.error(err));
+
+    this.props.homeState({
+      modalView: false
+    });
+  }
+
+  handleClickOff(e) {
+    this.props.homeState({
+      modalView: false
+    });
+  }
+
+  stopPropagation(e) {
+    e.stopPropagation();
+  }
+
+  render() {
+
+    if (!this.props.view) {
+      return null;
+    } else {
+      return (
+        <div className="overlay" onClick={this.handleClickOff}>
+          <div className="trip-modal" onClick={this.stopPropagation}>
+            <h2>Add a trip</h2>
+            <form id="tripForm" onSubmit={this.handleSubmit}>
+              <label htmlFor="tripName">Name of trip</label>
+              <input type="text" id="tripName" name="tripName" onChange={this.handleChange} value={this.state.tripName} required></input>
+              <label htmlFor="tripDestination">Destination</label>
+              <input type="text" id="tripDestination" name="tripDestination" onChange={this.handleChange} value={this.state.tripDestination} required></input>
+              <label htmlFor="depatureDate">Depature Date</label>
+              <input type="date" id="departureDate" name="departureDate" min={Date.now()} onChange={this.handleChange} value={this.state.departureDate} required></input>
+              <label htmlFor="returnDate">Return Date</label>
+              <input type="date" id="returnDate" name="returnDate" onChange={this.handleChange} value={this.state.returnDate} required></input>
+              <button className="rounded-lg tripsubmit" type="submit" value="Submit">Add this trip</button>
+            </form>
+          </div>
+        </div>
+      );
+    }
+  }
+}
+
+// function TripEntries(props) {
+//   return (
+//     <div className="container">
+//       <h1>Your Trips</h1>
+//     </div>
+//   );
+// }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,110 @@
 require('dotenv/config');
 const express = require('express');
-const staticMiddleware = require('./static-middleware');
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+const pg = require('pg');
+const ClientError = require('./client-error');
+const errorMiddleware = require('./error-middleware');
+const authorizationMiddleware = require('./authorization-middleware');
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL
+});
 
 const app = express();
 
-app.use(staticMiddleware);
+const jsonMiddleware = express.json();
+app.use(jsonMiddleware);
+
+app.post('/api/auth/sign-up', (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new ClientError(400, 'username and password are required fields');
+  }
+  argon2
+    .hash(password)
+    .then(hashedPassword => {
+      const sql = `
+        insert into "users" ("username", "hashedPassword")
+        values ($1, $2)
+        returning "userId", "username"
+      `;
+      const params = [username, hashedPassword];
+      return db.query(sql, params);
+    })
+    .then(result => {
+      const [user] = result.rows;
+      res.status(201).json(user);
+    })
+    .catch(err => next(err));
+});
+
+app.post('/api/auth/sign-in', (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new ClientError(401, 'invalid login');
+  }
+  const sql = `
+    select "userId",
+           "hashedPassword"
+      from "users"
+     where "username" = $1
+  `;
+  const params = [username];
+  db.query(sql, params)
+    .then(result => {
+      const [user] = result.rows;
+      if (!user) {
+        throw new ClientError(401, 'invalid login');
+      }
+      const { userId, hashedPassword } = user;
+      return argon2
+        .verify(hashedPassword, password)
+        .then(isMatching => {
+          if (!isMatching) {
+            throw new ClientError(401, 'invalid login');
+          }
+          const payload = { userId, username };
+          const token = jwt.sign(payload, process.env.TOKEN_SECRET);
+          res.json({ token, user: payload });
+        });
+    })
+    .catch(err => next(err));
+});
+
+// app.use(authorizationMiddleware);
+
+app.get('/api/trip', (req, res, next) => {
+  const sql = `
+    select *
+      from "trip"
+  `;
+  db.query(sql)
+    .then(result => res.json(result.rows))
+    .catch(err => next(err));
+});
+
+app.post('/api/trip', (req, res, next) => {
+  // const { userId } = req.user;
+  const { tripName, tripDestination, departureDate, returnDate, numberOfNights } = req.body;
+  if (!tripName || !tripDestination || !departureDate || !returnDate || !numberOfNights) {
+    throw new ClientError(400, 'One of the following fields are missing: name, destination, departureDate, returnDate, numberOfNights');
+  }
+  const sql = `
+    insert into "trip" ("name", "destination", "departureDate", "returnDate", "numberOfNights")
+    values ($1, $2, $3, $4, $5)
+    returning *
+  `;
+  const params = [/* userId, */tripName, tripDestination, departureDate, returnDate, numberOfNights];
+  db.query(sql, params)
+    .then(result => {
+      const [tripDetail] = result.rows;
+      res.status(201).json(tripDetail);
+    })
+    .catch(err => next(err));
+});
+
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console

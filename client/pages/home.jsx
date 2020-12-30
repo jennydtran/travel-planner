d711c512a5e198b97fac8eaa0868f0e94da414d9
Jenny Tran <jdtran.art@gmail.com>
2020-12-30 23:21:49 +0000

    Trip entry placeholder created with HTML, Bootstrap, CSS. Added if/else statement to decide whether or not to render trip entry based on if the list is empty or not.

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -23,7 +23,7 @@ export default class Home extends React.Component {
     return (
       <>
         <TopNav />
-        <HomeBody />
+        <HomeBody tripEntries={tripEntries}/>
         <BottomNav onClick={handleClickAdd}/>
         { modalView &&
           <TripModal view={modalView} tripEntries={tripEntries} homeState={this.setState}/>
@@ -35,7 +35,7 @@ export default class Home extends React.Component {
 
 function TopNav(props) {
   return (
-    <nav className="nav navbar-light static-fixed-top  align-items-center">
+    <nav className="nav navbar-light static-fixed-top align-items-center px-2">
       <div className="navbar-brand mx-auto">
         <svg className="icon" data-name="Livello 1" id="Livello_1" width="2rem" height="2rem" fill="000000" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg">
           <path d="M127.12,60.22,115.46,48.56h0L69,2.05a7,7,0,0,0-9.9,0L12.57,48.53h0L.88,60.22a3,3,0,0,0,4.24,4.24l6.57-6.57V121a7,7,0,0,0,7,7H46a7,7,0,0,0,7-7V81a1,1,0,0,1,1-1H74a1,1,0,0,1,1,1v40a7,7,0,0,0,7,7h27.34a7,7,0,0,0,7-7V57.92l6.54,6.54a3,3,0,0,0,4.24-4.24ZM110.34,121a1,1,0,0,1-1,1H82a1,1,0,0,1-1-1V81a7,7,0,0,0-7-7H54a7,7,0,0,0-7,7v40a1,1,0,0,1-1,1H18.69a1,1,0,0,1-1-1V51.9L63.29,6.29a1,1,0,0,1,1.41,0l45.63,45.63Z" />
@@ -50,18 +50,69 @@ function TopNav(props) {
   );
 }
 
-function HomeBody(props) {
-  return (
-    <main className="d-flex flex-column justify-content-between">
-      <div>
-        <p className="h6 m-0 mt-3">Hello, <strong>username</strong></p>
-        <p className="m-0 p-0 mt-2 small text-muted"><span>What are we planning today?</span></p>
-        <hr className="dmx-2 my-4 d-block border-0" />
-      </div>
+class HomeBody extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tripEntries: this.props.tripEntries
+    };
+  }
 
-      <div className="light-teal text-center"><h3>Looks like you don&#39;t<br />have any trips planned!</h3></div>
-      <div className="light-teal text-center"><p>Let&#39;s start planning!<br />Click the <strong>ADD</strong> button to add a trip.</p></div>
-    </main>
+  render() {
+    return (
+      <main className="d-flex flex-column">
+        <div>
+          <p className="h6 m-0 mt-3">Hello, <strong>username</strong></p>
+          <p className="m-0 p-0 mt-2 small text-muted"><span>What are we planning today?</span></p>
+          <hr className="dmx-2 my-4 d-block border-0" />
+        </div>
+
+        {this.state.tripEntries.length !== 0
+          ? <div className="container">
+              <h1 className="text-center light-teal">Your Trips</h1>
+              <TripEntries />
+            </div>
+          : <>
+              <div className="light-teal text-center d-flex justify-content-center align-items-center h-75">
+                <h3>Looks like you don&#39;t<br />
+                have any trips planned!</h3>
+              </div>
+              <div className="light-teal text-center">
+                <p>Let&#39;s start planning!<br />
+                Click the <strong>ADD</strong> button to add a trip.</p>
+              </div>
+            </>
+        }
+      </main>
+    );
+  }
+}
+
+function TripEntries(props) {
+  return (
+    <ul className="list-unstyled my-4">
+      <li>
+        <div className="trip-entry d-flex border rounded-lg shadow-sm py-4 px-4">
+          <div className="d-flex align-items-center shadow-sm rounded-lg mr-4 px-2 border">
+            <a>
+              <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="#FFAD0F" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+              </svg>
+            </a>
+          </div>
+          <div className="d-flex flex-column justify-content-center flex-grow-1">
+            <h4 className="m-0">Big Bear Trip</h4>
+            <p className="text-muted m-0">January 2021</p>
+          </div>
+          <div className="d-flex align-items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="#dbdbdb" className="icon bi bi-dash" viewBox="0 0 16 16">
+              <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
+            </svg>
+          </div>
+        </div>
+      </li>
+    </ul>
   );
 }
 

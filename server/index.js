e609132919a7f9093312c2a24ffbb2a711c3e83d
Jenny Tran <jdtran.art@gmail.com>
2020-12-30 02:14:19 +0000

    Fixed files that needed to be fixed. Added back express.static. Took out any commented code that wasn't needed. Link to own stylesheets is put after external stylesheets.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('dotenv/config');
 const express = require('express');
+const staticMiddleware = require('./static-middleware');
 const argon2 = require('argon2');
 const jwt = require('jsonwebtoken');
 const pg = require('pg');
@@ -12,6 +13,7 @@ const db = new pg.Pool({
 });
 
 const app = express();
+app.use(staticMiddleware);
 
 const jsonMiddleware = express.json();
 app.use(jsonMiddleware);
@@ -72,7 +74,7 @@ app.post('/api/auth/sign-in', (req, res, next) => {
     .catch(err => next(err));
 });
 
-// app.use(authorizationMiddleware);
+app.use(authorizationMiddleware);
 
 app.get('/api/trip', (req, res, next) => {
   const sql = `
@@ -85,17 +87,17 @@ app.get('/api/trip', (req, res, next) => {
 });
 
 app.post('/api/trip', (req, res, next) => {
-  // const { userId } = req.user;
+  const { userId } = req.user;
   const { tripName, tripDestination, departureDate, returnDate, numberOfNights } = req.body;
   if (!tripName || !tripDestination || !departureDate || !returnDate || !numberOfNights) {
     throw new ClientError(400, 'One of the following fields are missing: name, destination, departureDate, returnDate, numberOfNights');
   }
   const sql = `
-    insert into "trip" ("name", "destination", "departureDate", "returnDate", "numberOfNights")
-    values ($1, $2, $3, $4, $5)
+    insert into "trip" ("userId", "name", "destination", "departureDate", "returnDate", "numberOfNights")
+    values ($1, $2, $3, $4, $5, $6)
     returning *
   `;
-  const params = [/* userId, */tripName, tripDestination, departureDate, returnDate, numberOfNights];
+  const params = [userId, tripName, tripDestination, departureDate, returnDate, numberOfNights];
   db.query(sql, params)
     .then(result => {
       const [tripDetail] = result.rows;

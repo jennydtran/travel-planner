3c5217bab32d917d61e7ca206915415384d0b7e8
Jenny Tran <jdtran.art@gmail.com>
2020-12-31 05:28:12 +0000

    Made changes for pr. Div changed into a button so it's clickable (no longer wrapped in an 'a' element. Fixed issue with storing props in state.

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -54,51 +54,42 @@ function TopNav(props) {
           <path d="M127.12,60.22,115.46,48.56h0L69,2.05a7,7,0,0,0-9.9,0L12.57,48.53h0L.88,60.22a3,3,0,0,0,4.24,4.24l6.57-6.57V121a7,7,0,0,0,7,7H46a7,7,0,0,0,7-7V81a1,1,0,0,1,1-1H74a1,1,0,0,1,1,1v40a7,7,0,0,0,7,7h27.34a7,7,0,0,0,7-7V57.92l6.54,6.54a3,3,0,0,0,4.24-4.24ZM110.34,121a1,1,0,0,1-1,1H82a1,1,0,0,1-1-1V81a7,7,0,0,0-7-7H54a7,7,0,0,0-7,7v40a1,1,0,0,1-1,1H18.69a1,1,0,0,1-1-1V51.9L63.29,6.29a1,1,0,0,1,1.41,0l45.63,45.63Z" />
         </svg>
       </div>
-      <a href="#" className="nav-item">
+      <button className="bg-transparent p-0 nav-item">
         <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" fill="000000" className="icon bi bi-three-dots-vertical" viewBox="0 0 16 16">
           <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
         </svg>
-      </a>
+      </button>
     </nav>
   );
 }
 
-class HomeBody extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tripEntries: this.props.tripEntries
-    };
-  }
+function HomeBody(props) {
+  return (
+    <main className="d-flex flex-column pt-3">
+      <div>
+        <p className="h6 m-0 mt-3">Hello, <strong>username</strong></p>
+        <p className="m-0 p-0 mt-2 small text-muted"><span>What are we planning today?</span></p>
+        <hr className="dmx-2 my-4 d-block border-0" />
+      </div>
 
-  render() {
-    return (
-      <main className="d-flex flex-column pt-3">
-        <div>
-          <p className="h6 m-0 mt-3">Hello, <strong>username</strong></p>
-          <p className="m-0 p-0 mt-2 small text-muted"><span>What are we planning today?</span></p>
-          <hr className="dmx-2 my-4 d-block border-0" />
-        </div>
-
-        {this.props.tripEntries.length !== 0
-          ? <div className="container-sm">
-              <h1 className="text-center light-teal">Your Trips</h1>
-              <TripEntries tripEntries={this.props.tripEntries}/>
+      {props.tripEntries.length !== 0
+        ? <div className="container-sm">
+            <h1 className="text-center light-teal">Your Trips</h1>
+            <TripEntries tripEntries={props.tripEntries}/>
+          </div>
+        : <>
+            <div className="light-teal text-center d-flex justify-content-center align-items-center h-75">
+              <h3>Looks like you don&#39;t<br />
+              have any trips planned!</h3>
             </div>
-          : <>
-              <div className="light-teal text-center d-flex justify-content-center align-items-center h-75">
-                <h3>Looks like you don&#39;t<br />
-                have any trips planned!</h3>
-              </div>
-              <div className="light-teal text-center">
-                <p>Let&#39;s start planning!<br />
-                Click the <strong>ADD</strong> button to add a trip.</p>
-              </div>
-            </>
-        }
-      </main>
-    );
-  }
+            <div className="light-teal text-center">
+              <p>Let&#39;s start planning!<br />
+              Click the <strong>ADD</strong> button to add a trip.</p>
+            </div>
+          </>
+      }
+    </main>
+  );
 }
 
 function TripEntries(props) {
@@ -137,7 +128,8 @@ function TripEntry(trip) {
               <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
               <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
             </svg>
-        </div></a>
+          </div>
+        </a>
         <div className="d-flex flex-column justify-content-center flex-grow-1">
           <h4 className="ml-4 m-0">{name}</h4>
           <p className="text-muted small pt-1 ml-4 m-0">{`${month} ${year}`}</p>
@@ -157,12 +149,12 @@ function TripEntry(trip) {
 function BottomNav(props) {
   return (
     <footer className="container-xl footer d-flex justify-content-center align-items-center fixed-bottom px-1 w-100">
-      <div className="text-center">
-        <a href="#"><div className="icon bg-white rounded-circle" onClick={props.onClick}>
+      <div className="d-flex text-center">
+        <button className="icon bg-white rounded-circle p-1" onClick={props.onClick}>
           <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16">
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
           </svg>
-        </div></a>
+        </button>
       </div>
     </footer>
   );

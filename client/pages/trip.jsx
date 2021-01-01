3ad3b84cc2c1df25995a7185b3989f88a195e361
Jenny Tran <jdtran.art@gmail.com>
2021-01-01 00:58:57 +0000

    Page is rendered. Summary, middle, and bottom icons are there but layout still needs adjustments.

--- client/pages/trip.jsx
+++ client/pages/trip.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ThreeDotNavIcon, BackLeftIcon, CameraIcon } from '../components/svg';
+import { ThreeDotNavIcon, BackLeftIcon, CameraIcon, ChecklistIcon, TravelerIcon, AirplaneIcon, MapIcon, ActivitiesIcon, PackingIcon, AccommodationIcon} from '../components/svg';
 
 export default class TripSummary extends React.Component {
   render() {
@@ -23,7 +23,7 @@ function TopNav(props) {
           </button>
         </a>
         <div>
-          <p className="h4 m-0 mx-2 text-muted">Big Bear</p>
+          <p className="h5 m-0 mx-2 mt-1 text-grey">Big Bear</p>
         </div>
       </div>
       <button className="bg-transparent p-0 nav-item">
@@ -35,7 +35,7 @@ function TopNav(props) {
 
 function Body(props) {
   return (
-    <main className="bg-gradient d-flex flex-column align-items-center pt-3">
+    <main className="bg-gradient d-flex flex-column align-items-center px-4 pt-3">
       <div className="d-flex my-3">
         <CameraIcon />
         <h2 className="text-center text-white px-3">Snapshot</h2>
@@ -49,21 +49,85 @@ function Body(props) {
 
 function Summary(props) {
   return (
-    <div />
+    <div className="d-flex flex-column rounded-lg bg-white w-100 border p-4">
+      <div className="d-flex">
+        <div className="flex-fill"><p className="font-weight-bold">Destination</p></div>
+        <div className="flex-fill"><p className="light-teal">Big Bear, CA</p></div>
+      </div>
+      <div className="d-flex justify-content-between">
+        <div className="flex-fill"><p className="font-weight-bold">Departure Date</p></div>
+        <div className="flex-fill"><p className="light-teal">01/01/1001</p></div>
+      </div>
+      <div className="d-flex justify-content-between">
+        <div className="flex-fill"><p className="font-weight-bold">Return Date</p></div>
+        <div className="flex-fill"><p className="light-teal">01/01/1001</p></div>
+      </div>
+      <div className="d-flex justify-content-between">
+        <div className="flex-fill"><p className="font-weight-bold">Number of Days</p></div>
+        <div className="flex-fill"><p className="light-teal">5 days</p></div>
+      </div>
+    </div>
   );
 }
 
 function ToDoReminder(props) {
   return (
-    <div />
+    <div className="bg-white rounded-lg w-100 border p-4">
+      <p className="font-weight-bold">Before leaving on your trip:</p>
+      <div className="d-flex">
+        <div className="small light-teal">
+          <p>Looks like you don't have a checklist.</p>
+          <p>Click on the icon to start prepping<br />for your trip!</p>
+        </div>
+        <div>
+          <button className="bg-white rounded-lg p-2">
+            <ChecklistIcon />
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
 function BottomNav(props) {
   return (
-    <div>
-      <ul>
-      </ul>
+    <div className="d-flex flex-wrap">
+      <div >
+        <button className="bg-white rounded-lg p-3">
+          <TravelerIcon />
+        </button>
+        <p className="small">Travelers</p>
+      </div>
+      <div >
+        <button className="bg-white rounded-lg p-3">
+          <AirplaneIcon />
+        </button>
+        <p className="small">Transportation</p>
+      </div>
+      <div >
+        <button className="bg-white rounded-lg p-3">
+          <AccommodationIcon />
+        </button>
+        <p className="small">Accommodation</p>
+      </div>
+      <div >
+        <button className="bg-white rounded-lg p-3">
+          <ActivitiesIcon />
+        </button>
+        <p className="small">Activities</p>
+      </div>
+      <div >
+        <button className="bg-white rounded-lg p-3">
+          <MapIcon />
+        </button>
+        <p className="small">Places</p>
+      </div>
+      <div >
+        <button className="bg-white rounded-lg p-3">
+          <PackingIcon />
+        </button>
+        <p className="small">Packing List</p>
+      </div>
     </div>
   );
 }

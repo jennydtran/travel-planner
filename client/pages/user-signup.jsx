6f032f7f9dfed9d72991a91f645642f462455648
Jenny Tran <jdtran.art@gmail.com>
2021-01-07 03:55:00 +0000

    changed some text to dark-teal

--- client/pages/user-signup.jsx
+++ client/pages/user-signup.jsx
@@ -19,13 +19,13 @@ export default class UserSignUp extends React.Component {
             <form id="signup" className="px-3 d-flex flex-column">
 
               {/* <div className="form-group">
-                <label className="light-teal mb-3" htmlFor="username">Username</label>
+                <label className="dark-teal mb-3" htmlFor="username">Username</label>
                 <input className="form-control form-control-lg mb-3" type="text" id="username" name="username" required />
               </div>
               <button className="w-100 rounded-lg align-self-center my-3 mt-5" type="submit" value="Submit">Next</button> */}
 
               <div className="form-group">
-                <label className="light-teal mb-3" htmlFor="password">Password</label>
+                <label className="dark-teal mb-3" htmlFor="password">Password</label>
                 <div className="d-flex justify-content-end">
                   <input className="form-control form-control-lg mb-3" type="password" id="password" name="password" required />
                   <button className="nofilter p-0 bg-transparent position-absolute mt-2 mr-3">
@@ -39,7 +39,7 @@ export default class UserSignUp extends React.Component {
             </form>
 
             {/* <div className="text-center dark-teal">
-              <h5 className="font-weight-bold mt-4">Thank you</h5>
+              <h5 className="dark-teal font-weight-bold mt-4">Thank you</h5>
               <p className="mt-3 mb-1">Account successfully created.</p>
               <p className="mt-2">Please sign in to start travel planning!</p>
               <button className="w-100 rounded-lg align-self-center my-3 mt-4" type="submit" value="Submit">Finish</button>

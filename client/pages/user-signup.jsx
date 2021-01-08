cb98cffaa227c741235a768ecad9a313cd63d485
Jenny Tran <jdtran.art@gmail.com>
2021-01-08 01:00:45 +0000

    cleaned up signup component. split into componenet files

--- client/pages/user-signup.jsx
+++ client/pages/user-signup.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Logo from '../components/logo';
-import { CircleInactive, CircleActive, EyeClosed, EyeOpen } from '../components/svg';
+import InputUsername from '../components/validation-username';
+import InputPassword from '../components/validation-password';
+import { CircleInactive, CircleActive } from '../components/svg';
 
 export default class UserSignUp extends React.Component {
   constructor(props) {
@@ -60,7 +62,7 @@ export default class UserSignUp extends React.Component {
         this.setState({
           username: '',
           password: '',
-          currentView: 'Finished'
+          currentView: 'finished'
         });
       })
       .catch(err => console.error(err));
@@ -90,115 +92,6 @@ export default class UserSignUp extends React.Component {
             <a href="" className="signin-up">Sign In</a>
           </div>
         </main>
-      </>
-    );
-  }
-}
-
-class InputUsername extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.validateUsername = this.validateUsername.bind(this);
-  }
-
-  validateUsername(inputvalue) {
-    const regexAlphaNumDash = /[^a-zA-Z0-9_]/;
-    const regexFour = /^(?=.{4,})/;
-
-    if (inputvalue === '') {
-      this.props.handleUsername(inputvalue, 'A username is required.');
-    } else if (regexFour.test(inputvalue) === false) {
-      this.props.handleUsername(inputvalue, 'Your username must be at least 4 characters long.');
-    } else if (regexAlphaNumDash.test(inputvalue) === true) {
-      this.props.handleUsername(inputvalue, 'Your username can only contain letters, numbers, and underscore.');
-    } else {
-      this.props.handleUsername(inputvalue, '');
-    }
-  }
-
-  handleChange(event) {
-    this.validateUsername(event.target.value);
-  }
-
-  render() {
-    return (
-      <>
-        <div className="form-group">
-          <label className="dark-teal mb-3" htmlFor="username">Username</label>
-          <input className="form-control form-control-lg mb-2" type="text" id="username" name="username" value={this.props.username} onChange={this.handleChange} required />
-          <p className="text-center text-danger small">{this.props.error}</p>
-        </div>
-        <button className="w-100 rounded-lg align-self-center my-3 mt-5" onClick={this.props.clickNext}>Next</button>
-      </>
-    );
-  }
-}
-
-class InputPassword extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      type: 'password',
-      hidden: false
-    };
-    this.passwordToggle = this.passwordToggle.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.validatePassword = this.validatePassword.bind(this);
-  }
-
-  passwordToggle(e) {
-    e.preventDefault();
-    const hidden = this.state.hidden;
-    const type = hidden ? 'password' : 'text';
-    this.setState({
-      type: type,
-      hidden: !hidden
-    });
-  }
-
-  validatePassword(inputvalue) {
-    const regexLowercase = /^(?=.*[a-z])/;
-    const regexUppercase = /^(?=.*[A-Z])/;
-    const regexNumber = /^(?=.*[0-9])/;
-    const regexSpecial = /^(?=.*[!@#$%^&*()])/;
-    const regexEight = /^(?=.{8,})/;
-
-    if (inputvalue === '') {
-      this.props.handlePassword(inputvalue, 'A password is required.');
-    } else if (regexEight.test(inputvalue) === false) {
-      this.props.handlePassword(inputvalue, 'Your password must be at least 8 characters long.');
-    } else if (regexSpecial.test(inputvalue) === false) {
-      this.props.handlePassword(inputvalue, 'Your password must contain at least one special character.');
-    } else if (regexNumber.test(inputvalue) === false) {
-      this.props.handlePassword(inputvalue, 'Your password must contain at least one number.');
-    } else if (regexUppercase.test(inputvalue) === false) {
-      this.props.handlePassword(inputvalue, 'Your password must contain at least one uppercase letter.');
-    } else if (regexLowercase.test(inputvalue) === false) {
-      this.props.handlePassword(inputvalue, 'Your password must contain at least one lowercase letter.');
-    } else {
-      this.props.handlePassword(inputvalue, '');
-    }
-  }
-
-  handleChange(event) {
-    this.validatePassword(event.target.value);
-  }
-
-  render() {
-    return (
-      <>
-        <div className="form-group">
-          <label className="dark-teal mb-3" htmlFor="password">Password</label>
-          <div className="d-flex justify-content-end">
-            <input className="form-control form-control-lg mb-2" type={this.state.type} id="password" name="password" value={this.props.password} onChange={this.handleChange} required />
-            <button className="nofilter p-0 bg-transparent position-absolute mt-2 mr-3" onClick={this.passwordToggle}>
-              {!this.state.hidden ? <EyeOpen /> : <EyeClosed /> }
-            </button>
-          </div>
-          <p className="text-center text-danger small">{this.props.error}</p>
-        </div>
-        <button className="w-100 rounded-lg align-self-center my-3 mt-5" type="submit" value="Submit">Create Account</button>
       </>
     );
   }
